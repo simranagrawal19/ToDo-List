31b304f5c7388caff0167ac806aa82a351f2486c
Simran <simranagrawal193@gmail.com>
2022-06-04 09:27:27 +0530

    Initial commit

---
+++ code.js
@@ -0,0 +1,64 @@
+//Create a "close" button and append it to each list item
+
+var myList = document.getElementsByTagName("li");
+
+var i;
+for (i = 0; i < myList.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myList[i].appendChild(span);
+}
+
+//Click on a close button to hide current list item
+var close = document.getElementsByClassName("close");
+
+var i;
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+//add a "checked" symbol when clicking on the next item
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+//create a new list item when clicking on the "add" button
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue == '') {
+        alert('You must write something!');
+    } else {
+        document.getElementById('myUL').appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+
+
+
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
